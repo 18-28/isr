5ffe90628247078a3af92c71d63c6ef41ed87efe
zacharysutton-bg <108938149+zacharysutton-bg@users.noreply.github.com>
2024-06-25 15:53:47 +0100

    add component

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,26 @@
-// import Image from "next/image";
-// import { Inter } from "next/font/google";
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
-// const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
-// export default function Home() {
-//   return (
-//     <>
-    
-//     </>
+export default function Home(props) {
+  console.log(props)
+  return (
+    <>
+      <h1>home</h1>
+    </>
 
 
-// );
-// }
+);
+}
 
 // export async function getStaticProps(){
 //   const data = await fetch('https://polite-stone-0e6ccff03.4.azurestaticapps.net/api/storyblok')
+//   const json = {data: }
+
+//   return {
+//     props: {
+//       data
+//     }
+//   }
 // }
