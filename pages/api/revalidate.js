460135d2416a51262f2f58763c8ca01c693d2b22
zacharysutton-bg <108938149+zacharysutton-bg@users.noreply.github.com>
2024-06-25 16:17:03 +0100

    hard code reval

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,8 +1,8 @@
 export default async function revalidate(req, res){
     try {
-        console.log(`revalidating: ${req.query.slug}`)
-        await res.revalidate(req.query.slug)
-        return res.status(200).json({ message: `Revalidated: ${req.query.slug}` })
+        console.log(`revalidating: /`)
+        await res.revalidate('/')
+        return res.status(200).json({ message: `Revalidated: /` })
     } catch (error) {
         return res.status(500).json({ error: error.message })
     }
