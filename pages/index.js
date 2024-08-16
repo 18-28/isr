a846cac382b5ba0ed315d2f830e19365de0659b6
Zachary Sutton <zachary-sutton@outlook.com>
2024-08-16 14:37:29 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import StoryblokClient from 'storyblok-js-client'
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home(props) {
-
+  console.log(props)
   return (
     <>
       <h1>{props.data.data.story.content.body[0].text}</h1>
